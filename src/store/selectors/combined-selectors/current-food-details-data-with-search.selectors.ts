eb3a7b815d7f4d39915714bda925a400a2159a5f
chrisberry4545 <chrisberry4545@googlemail.com>
2019-06-15 12:12:00 +0100

    added selector for searching recipes on food details

---
+++ src/store/selectors/combined-selectors/current-food-details-data-with-search.selectors.ts
@@ -0,0 +1,15 @@
+
+import { createSelector } from 'reselect';
+import { selectCurrentSearchTerm } from '../ui.selectors';
+import { IRecipe } from '../../../interfaces';
+import { getDataThatContainsSearchTerm } from '../../../helpers';
+import { selectCurrentFoodDetailsRecipes } from '../current-food-details-data.selectors';
+
+export const selectVisibleRecipesForFoodDetailsData = createSelector(
+  selectCurrentSearchTerm,
+  selectCurrentFoodDetailsRecipes,
+  (searchTerm, recipes): IRecipe[] | undefined => getDataThatContainsSearchTerm(
+    searchTerm,
+    recipes
+  )
+);
--- src/store/selectors/combined-selectors/index.ts
+++ src/store/selectors/combined-selectors/index.ts
@@ -2,4 +2,6 @@ export * from './all-seasons-with-food-and-search.selectors';
 
 export * from './current-food-data-and-basic-season.selectors';
 
+export * from './current-food-details-data-with-search.selectors';
+
 export * from './current-season-data-with-search.selectors';
